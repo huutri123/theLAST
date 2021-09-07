3aa42db74211ea76fe009e1b0c456af3a972fa04
NghiaHackerGa <tuannghia444hbtqn@gmail.com>
2021-09-07 10:38:59 +0700

    oh yeah

--- assets/JS/operation.js
+++ assets/JS/operation.js
@@ -4,13 +4,23 @@ window.onscroll = () => {
 
 function navbarAdaptation() {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        document.getElementById("header").style.backgroundColor = "#ccc";
-        document.getElementsByClassName("header__logo-img")[0].src = "./assets/img/logo(scrolled).png";
-        document.getElementsByClassName("header__logo-img")[0].style.width = "117px";   
+        document.querySelector("#header").style.backgroundColor = "var(--white)";
+        document.querySelector("#header").style.boxShadow = "0 2px 10px -1px rgb(87 97 100 / 35%)";
+        document.querySelector(".header__logo img").src = "./assets/img/logo(scrolled).png";
+        document.querySelector(".header__logo img").style.width = "117px";
+        [...(document.querySelectorAll(".header__nav-item a"))].map(args => {
+            args.style.color = "var(--black)"
+        })
     } else {
-        document.getElementById("header").style.backgroundColor = "";
-        document.getElementsByClassName("header__logo-img")[0].src = "./assets/img/logo.png";
-        document.getElementsByClassName("header__logo-img")[0].style.width = "143px";
+        document.querySelector("#header").style.backgroundColor = "";
+        document.querySelector("#header").style.height = "74px";
+        document.querySelector("#header").style.boxShadow = "none";
+        document.querySelector(".header__logo img").src = "./assets/img/logo.png";
+        document.querySelector(".header__logo img").style.width = "143px";
+        [...(document.querySelectorAll(".header__nav-item a"))].map(args => {
+            args.style.color = "var(--white)"
+        })
     }
 }
 
+// cay vl
