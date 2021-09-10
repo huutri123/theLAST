bd8d8fa13e0a159dfe056666a4fb15612c0cc933
NghiaHackerGa <tuannghia444hbtqn@gmail.com>
2021-09-10 08:19:29 +0700

    add all file

--- assets/JS/operation.js
+++ assets/JS/operation.js
@@ -6,8 +6,8 @@ function navbarAdaptation() {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
         document.querySelector("#header").style.backgroundColor = "var(--white)";
         document.querySelector("#header").style.boxShadow = "0 2px 10px -1px rgb(87 97 100 / 35%)";
+        document.querySelector(".header__logo img").style.transform = "scale(0.8) translate(-10%, -10%)";
         document.querySelector(".header__logo img").src = "./assets/img/logo(scrolled).png";
-        document.querySelector(".header__logo img").style.width = "117px";
         [...(document.querySelectorAll(".header__nav-item a"))].map(args => {
             args.style.color = "var(--black)"
         })
@@ -15,8 +15,8 @@ function navbarAdaptation() {
         document.querySelector("#header").style.backgroundColor = "";
         document.querySelector("#header").style.height = "74px";
         document.querySelector("#header").style.boxShadow = "none";
+        document.querySelector(".header__logo img").style.transform = "scale(1)";
         document.querySelector(".header__logo img").src = "./assets/img/logo.png";
-        document.querySelector(".header__logo img").style.width = "143px";
         [...(document.querySelectorAll(".header__nav-item a"))].map(args => {
             args.style.color = "var(--white)"
         })
